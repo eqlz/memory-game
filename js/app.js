2e854ccfde3015c73cc0432e29d93341c878d6d3
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-04-16 21:59:12 -0400

    add congrats modal structures in html, css, js

--- js/app.js
+++ js/app.js
@@ -268,6 +268,8 @@ function cardsAllMatched() {
         //alert("cards all matched!");
         //displayCards(allCardsSymbol);
         timerStop();
+        //$('#myModal').modal('show');
+        congratulationsPopup();
     } 
 }
 
@@ -322,10 +324,68 @@ function incrementTimer() {
 }
 /**/
 
-
-
 //*
 function timerStop() {
     clearInterval(timerInterval);
 }
 /**/
+
+/*
+function congratulationsPopup() {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+/**/
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var message = document.getElementsByClassName("modal-message")[0];
+
+
+function congratulationsPopup() {
+// When the user clicks on the button, open the modal 
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
